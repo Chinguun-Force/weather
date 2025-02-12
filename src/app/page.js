ed84629c11c6498a1e77a80f1e5a12fe5d7fff17
24LP4066 24LP4066 <24LP4066@G303-CC0144.local>
2025-02-12 12:35:09 +0800

    testeed

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,22 @@
 
 import Search from "@/components/Search";
 import Logo from "@/components/Logo";
+import Sun from "@/components/Sun";
 
 export default function Home() {
   return (
-    <div className="flex h-screen">
-        <div className='w-1/2 bg-[#F3F4F6] p-40'>
-        <Search/>
-        {/* <Logo/> */}
+    <div className="relative">
+      
+      <div className="flex h-screen">
+        <div className='w-1/2 bg-[#F3F4F6] p-28 relative'>
+          <Search />
+          <Sun/>
+          <div className="w-[140px] h-[140px] rounded-full bg-[#F3F4F6] absolute -right-[70px] top-[43%] border"></div>
         </div>
         <div className='w-1/2 bg-[#0F141E]'>
         </div>
+      </div>
+
     </div>
   );
 }
