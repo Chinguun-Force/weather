b178cbfd790bd260f3d2dd74848d8cbfcf1ce2d0
24LP4066 24LP4066 <24LP4066@G303-CC0144.local>
2025-02-13 12:11:01 +0800

    style done almost

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,57 @@
 
 import Search from "@/components/Search";
 import Logo from "@/components/Logo";
-import Sun from "@/components/Sun";
+import Card from "@/components/Card";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Home() {
+  const bgClipText = [
+    "[#111827]", "[#6B7280]"
+  ]
+  const [Data, setData] = useState([])  
+  useEffect(() => {
+  async function getWeather() {
+    const response = await fetch("https://countriesnow.space/api/v0.1/countries")
+    const fetchedData = await response.json()
+    const dataArr = fetchedData.data
+    const countries = dataArr.map(each => each.country)
+    const citiesArr = dataArr.map(each => each.cities)
+    const cityList = Object.values(citiesArr).flatMap(each => each)
+    setData(cityList)
+    console.log(cityList)
+    return cityList
+  }
+  getWeather()}, [])
   return (
     <div className="relative">
       
       <div className="flex h-screen">
-        <div className='w-1/2 bg-[#F3F4F6] p-28 relative'>
-          <Search />
-          <Sun/>
+        <div className='w-1/2 bg-[#F3F4F6] flex flex-col justify-end pb-32 items-center relative'>
+          <Search 
+            cityList = {Data}
+          />
+          <Card 
+            imgUrl = "https://pinecone-academy-weather-app.vercel.app/_next/image?url=%2Fsun.png&w=640&q=75"
+            weatherStatusClass = "text-[#FF8E27] text-xl font-bold"
+            activeButton = "active:text-black"
+            bg = "bg-[#F9FAFB]"
+            text = "text-black"
+            gradient = {bgClipText}
+            gradientDirection = "b"
+          />
           <div className="w-[140px] h-[140px] rounded-full bg-[#F3F4F6] absolute -right-[70px] top-[43%] border"></div>
         </div>
-        <div className='w-1/2 bg-[#0F141E]'>
+        <div className='w-1/2 bg-[#0F141E] flex justify-center items-end pb-32 relative'>
+        <Card 
+            imgUrl = "https://pinecone-academy-weather-app.vercel.app/_next/image?url=%2Fmoon.png&w=640&q=75"
+            weatherStatusClass = "text-[#FF8E27] text-xl font-bold"
+            activeButton = "active:text-black"
+            bg = "bg-[#111827]"
+            text = "text-white"
+            gradient = {bgClipText}
+            gradientDirection = "t"
+          />
         </div>
       </div>
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,21 @@
-export default function Search() {
+export default function Search(props) {
+    // console.log(props.cityList)
     return (
-        <div className="w-[567px] h-[80px] rounded-full flex items-center bg-white px-[24px] py-[18px] gap-4">
-            <img
-                src="https://res.cloudinary.com/dl3wkodkk/image/upload/v1739152680/Weather%20API/search-svgrepo-com_f5yguv.svg"
-                className="w-[35px] h-[35px]"
-            />
-            <input placeholder="Search..." className="w-full focus:outline-none text-[32px] font-semibold"></input>
+        <div className="max-w-[567px] max-h-[80px] rounded-full flex items-center bg-white px-[24px] py-[18px] gap-4">
+            <div className="flex items-center gap-4 max-w-[567px] max-h-[80px]">
+                <img
+                    src="https://res.cloudinary.com/dl3wkodkk/image/upload/v1739152680/Weather%20API/search-svgrepo-com_f5yguv.svg"
+                    className="w-[35px] h-[35px]"
+                />
+                <input placeholder="Search..." className="w-full focus:outline-none text-[32px] font-semibold"></input>
+            </div>
+            <div>
+                {props.cityList.map((city, index) => (
+                    <div key={index} className="">
+                        {city}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
